dbf8fd226224168910dc26299379ec5811967a3a
Krishna Kolte <krishnadk8203@gmail.com>
2023-03-15 22:21:05 +0530

    user addition and edit ui added

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -3,31 +3,73 @@ import { useState } from "react";
 import Apiservice from "../Apiservice";
 import "../Styles/ContributeStyle.css";
 
-export default function AddUser(props) {
-    const user = props.user;
-    const [name, setName] = useState(null);
-    const [rollNo, setRollNo] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [role, setRole] = useState(null);
+export default function AddUser() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const [name, setName] = useState("");
+    const [rollNo, setRollNo] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [instiMail, setInstiMail] = useState("");
+    const [role, setRole] = useState("");
     const [checked, setChecked] = useState(false);
+    const [error, setError] = useState(null);
+    const [display, setDisplay] = useState("none");
+    const [alert, setAlert] = useState("none");
+
+    const resetData = () =>{
+        setName("");
+        setRollNo("");
+        setPassword("");
+        setPhone("");
+        setEmail("");
+        setInstiMail("");
+        setRole("");
+        setChecked(false);
+        setError(null);
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if(role===null){
-            setRole(user.role);
-        }
-        if(checked){
+        if (checked) {
             try {
-                await axios.post(Apiservice + "/signin", {
-                    name: name,
-                    role: role,
-                    rollNo: rollNo,
-                    password: password
-                }).then((res)=>{
-                    console.log(res.data)
-                })
+                if (role === "") {
+                    await axios.post(Apiservice + "/signin", {
+                        name: name,
+                        role: user.role,
+                        rollNo: rollNo,
+                        password: password,
+                        phone: phone,
+                        email: email,
+                        instiMail: instiMail,
+                        createdBy: user.rollNo // check this piece of code here
+                    }).then((res) => {
+                        console.log(res);
+                        setError(null);
+                        setDisplay("block");
+                        resetData();
+                    })
+                } else {
+                    await axios.post(Apiservice + "/signin", {
+                        name: name,
+                        role: role,
+                        rollNo: rollNo,
+                        password: password,
+                        phone: phone,
+                        email: email,
+                        instiMail: instiMail,
+                        createdBy: user.rollNo // check this piece of code here
+                    }).then((res) => {
+                        console.log(res);
+                        setError(null);
+                        setDisplay("block");
+                        resetData();
+                    })
+                }
             } catch (error) {
-                console.log(error)
+                console.log(error);
+                setError(error.response.data);
+                setAlert("block");
             }
         }
     }
@@ -35,36 +77,65 @@ export default function AddUser(props) {
     return (
         <>
             <div className="adduser-container">
-                <div style={{ "width": "60%", "margin": "0 auto", "color": "rgba(253, 160, 133, 1)" }}>
+                <div className="adduser-container-div">
                     {user !== null && <form onSubmit={handleSubmit}>
                         <div style={{ "display": "flex", "alignItems": "center" }}>
-                            <div className="form-group" style={{ "width": "60%", "display": "flex", "alignItems": "center", "justifyContent": "end" }}>
+                            <div className="form-group">
                                 <label htmlFor="exampleInputEmail1">Role &emsp;</label>
                                 {user.role === "Owner" && <select className="form-control" id="exampleFormControlSelect1" style={{ "width": "60%", "color": "#000000" }} onChange={(e => setRole(e.target.value))} required>
-                                    <option value={null}>-SELECT-</option>
+                                    <option value="">-SELECT-</option>
                                     <option value="Admin1">Admin1</option><option value="Admin2">Admin2</option>
                                     <option value="Admin3">Admin3</option><option value="Admin4">Admin4</option>
                                     <option value="Admin5">Admin5</option>
                                 </select>}
                                 {user.role !== "Owner" && <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={user.role} style={{ "width": "60%" }} disabled />}
                             </div>
-                            <div className="form-group" style={{ "width": "60%", "display": "flex", "alignItems": "center", "justifyContent": "end" }}>
+                            <div className="form-group">
                                 <label htmlFor="exampleInputEmail2">Name &emsp;</label>
                                 <input type="text" className="form-control" id="exampleInputEmail2" aria-describedby="emailHelp" style={{ "width": "60%" }} placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
                             </div>
                         </div>
                         <div style={{ "display": "flex", "alignItems": "center" }}>
-                            <div className="form-group" style={{ "width": "60%", "display": "flex", "alignItems": "center", "justifyContent": "end" }}>
+                            <div className="form-group">
                                 <label htmlFor="exampleInputEmail3">Roll Number &emsp;</label>
                                 <input type="text" className="form-control" placeholder='Roll Number' value={rollNo} onChange={(e) => setRollNo(e.target.value)} id="exampleInputEmail3" aria-describedby="emailHelp" style={{ "width": "60%", "color": "#000000" }} required />
                             </div>
-                            <div className="form-group" style={{ "width": "60%", "display": "flex", "alignItems": "center", "justifyContent": "end" }}>
-                                <label htmlFor="exampleInputEmail4">Password &emsp;</label>
-                                <input type="password" className="form-control" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} id="exampleInputEmail4" aria-describedby="emailHelp" style={{ "width": "60%", "color": "#000000" }} required />
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail4">Personal Mail &emsp;</label>
+                                <input type="email" className="form-control" placeholder='Personal Mail' value={email} onChange={(e) => setEmail(e.target.value)} id="exampleInputEmail4" aria-describedby="emailHelp" style={{ "width": "60%", "color": "#000000" }} required />
                             </div>
                         </div>
-                        <input type="checkbox" name="anonymous" onChange={(e) => setChecked(e.target.checked)} id="anonymous" required/><label htmlFor='anonymous'>&ensp;<i>(You must add an admin responsibly!!)</i></label>
-                        <span style={{ "display": "flex", "justifyContent": "center" }}><button type="submit" className="btn btn-primary" style={{ "textAlign": "center", "width": "20%" }}>Create</button></span>
+                        <div style={{ "display": "flex", "alignItems": "center" }}>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail5">Insti Mail &emsp;</label>
+                                <input type="email" className="form-control" placeholder='Insti Mail' value={instiMail} onChange={(e) => setInstiMail(e.target.value)} id="exampleInputEmail5" aria-describedby="emailHelp" style={{ "width": "60%", "color": "#000000" }} required />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail6">Phone &emsp;</label>
+                                <input type="tel" className="form-control" placeholder='Phone Number' value={phone} onChange={(e) => setPhone(e.target.value)} id="exampleInputEmail6" aria-describedby="emailHelp" style={{ "width": "60%", "color": "#000000" }} required />
+                            </div>
+                        </div>
+                        <div style={{ "display": "flex", "alignItems": "center" }}>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail7">Password &emsp;</label>
+                                <input type="password" className="form-control" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} id="exampleInputEmail7" aria-describedby="emailHelp" style={{ "width": "60%", "color": "#000000" }} required />
+                            </div>
+                        </div>
+                        <input type="checkbox" name="anonymous" onChange={(e) => setChecked(e.target.checked)} id="anonymous" required /><label htmlFor='anonymous'>&ensp;<i>(You must add an admin responsibly, and you'll be equally responsible if any mishap is caused by the admin created by you!!)</i></label>
+                        {error && <><div style={{ "color": "red", "textAlign": "center" }}>** {error}</div><br /></>}
+                        <div className="alert alert-success alert-dismissible fade show" role="alert" style={{ "display": `${display}`, "margin": "0 auto" }}>
+                            <strong>Done!!</strong> New admin created successfully.
+                            <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => setDisplay("none")}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="alert alert-danger alert-dismissible fade show" role="alert" style={{ "display": `${alert}`, "margin": "0 auto" }}>
+                            <strong>Oops!!</strong> Unable to create new user. Try again later.
+                            <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => setAlert("none")}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <span style={{ "display": "flex", "justifyContent": "center" }}><button type="submit" className="btn btn-primary">Create</button></span>
                     </form>}
                 </div>
             </div>
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,47 +1,148 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Styles/ProfileStyle.css";
-import address from"../images/avatar.png"
-import { useAuthContext } from '../hooks/useAuthContext';
+import address from "../images/avatar.png"
+import axios from 'axios';
+import Apiservice from '../Apiservice';
 
 function Profile() {
-    const { user } = useAuthContext();
-    if(user){
+    const user = JSON.parse(localStorage.getItem("user"));
+    const [edit, setEdit] = useState(false);
+    const [name, setName] = useState(user.name);
+    const [phone, setPhone] = useState(user.phone);
+    const [mail, setMail] = useState(user.email);
+    const [imail, setImail] = useState(user.instiMail)
+    const [fb, setFb] = useState(user.fbLink);
+    const [lid, setLid] = useState(user.linkedinLink);
+    const [image, setImage] = useState(user.image);
+    const [display, setDisplay] = useState("none");
+    const [alert, setAlert] = useState("none");
+
+    const exitEdit = () => {
+        setEdit(false);
+        setName(user.name);
+        setPhone(user.phone);
+        setMail(user.email);
+        setImail(user.instiMail);
+        setFb(user.fbLink);
+        setLid(user.linkedinLink);
+    }
+
+    const confirmEdit = async () => {
+        try {
+            await axios.patch(Apiservice + "/signin", {
+                name: name,
+                phone: phone,
+                email: mail,
+                instiMail: imail,
+                fbLink: fb,
+                linkedinLink: lid,
+                image: image
+            }, {
+                headers: {
+                    "Authorization": `Bearer ${user.token}`
+                }
+            }).then((res)=>{
+                (res.status===200)?setDisplay("block"):setAlert("block");
+                exitEdit();
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    if (user) {
+        console.log(user);
         return (
             <>
                 <section className="vh-100" style={{ backgroundColor: 'black' }}>
+                    <div className="alert alert-success alert-dismissible fade show" role="alert" style={{"display": `${display}`, "width": "60%", "margin": "0 auto"}}>
+                        <strong>Done!!</strong> Changes will be visible from your next login.
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={()=> setDisplay("none")}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="alert alert-danger alert-dismissible fade show" role="alert" style={{"display": `${alert}`, "width": "60%", "margin": "0 auto"}}>
+                        <strong>Oops!!</strong> Unable to edit. Try again later.
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={()=>setAlert("none")}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
                     <div className="container py-5 h-100">
                         <div className="row d-flex justify-content-center align-items-center h-100">
                             <div className="col col-lg-6 mb-4 mb-lg-0">
-                                <div className="card mb-3" style={{ "borderRadius": '.5rem'}}>
+                                <div className="card mb-3" style={{ "borderRadius": '.5rem', "width": "auto", "backgroundColor": "whitesmoke" }}>
                                     <div className="row g-0">
-                                        <div className="col-md-4 gradient-custom text-center text-white" style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
-                                            <img src={address} alt="Avatar" className="img-fluid my-5" style={{ width: '22vh' }} />
-                                            <h5>{user.name}</h5>
-                                            <p>{user.rollNo}</p>
+                                        <div className="col-md-4 gradient-custom text-center text-white" style={{ borderRadius: '.5rem' }}>
+                                            <img src={address} alt="Avatar" className="img-fluid-my-5"/>
+                                            {user.name && <h5>{user.name}</h5>}
+                                            {user.rollNo && <p>{user.rollNo}</p>}
+                                            <p>{user.role}</p> <br />
+                                            {!edit && <p style={{ "color": "#28a745" }} className="admin-profile-edit" onClick={() => setEdit(true)}>
+                                                Edit&nbsp;
+                                                <i className="fa-regular fa-pen-to-square fa-lg" style={{ "margin": "0 0.8rem 0 0", "color": "#28a745" }} />
+                                            </p>}
+                                            {edit && <p>
+                                                <i className="fa-solid fa-square-check actions-icon fa-xl" style={{ "margin": "0 0.8rem 0 0", "color": "#28a745" }}
+                                                    data-toggle="tooltip" title="Confirm" onClick={() => {
+                                                        confirmEdit();
+                                                    }}></i>
+                                                <i className="fa-solid fa-circle-xmark actions-icon fa-xl" style={{ "margin": "0 0.8rem 0 0", "color": "#ff2525" }}
+                                                    data-toggle="tooltip" title="Exit" onClick={() => {
+                                                        exitEdit();
+                                                    }}></i>
+                                            </p>}
                                         </div>
                                         <div className="col-md-8">
                                             <div className="card-body p-4">
                                                 <h6>Information</h6>
                                                 <hr className="mt-0 mb-4" />
-                                                <div className="row pt-1">
-                                                    <div className="col-6 mb-3">
-                                                        <p className="text-muted"><span style={{"fontSize": "1.0rem"}}>Email : </span>{user.name}</p>
-                                                    </div> <br />
-                                                    <div className="col-6 mb-3">
-                                                        <p className="text-muted"><span style={{"fontSize": "1.0rem"}}>Phone</span> : {user.name}</p>
+                                                <div className="">
+                                                    <div className="">
+                                                        <p className="text-muted"><span style={{ "fontSize": "1rem", "color": "black" }}>Name : </span>
+                                                            {!edit && user.name}
+                                                            {edit && <input type="text" value={name} onChange={(e) => setName(e.target.value)} />}
+                                                        </p>
+                                                    </div>
+                                                    <div className="mb-3">
+                                                        <p className="text-muted"><span style={{ "fontSize": "1.0rem", "color": "black" }}>Phone</span> :
+                                                            {!edit && user.phone}
+                                                            {edit && <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} />}
+                                                        </p>
                                                     </div>
                                                 </div>
                                                 <h6>Additional Details</h6>
                                                 <hr className="mt-0 mb-4" />
-                                                <div className="row pt-1">
-                                                    <div className="col-6 mb-3">
+                                                <div className="">
+                                                    <div className="">
                                                         <h6>Institute Email</h6>
-                                                        <p className="text-muted">{user.instiMail}</p>
+                                                        <p className="text-muted">
+                                                            {!edit && user.instiMail}
+                                                            {edit && <input type="text" value={imail} onChange={(e) => setImail(e.target.value)} />}
+                                                        </p>
                                                     </div>
-                                                    {/* <div className="col-6 mb-3">
-                                                        <h6>Most Viewed</h6>
-                                                        <p className="text-muted">Dolor sit amet</p>
-                                                    </div> */}
+                                                    <div className="">
+                                                        <h6>Personal Email</h6>
+                                                        <p className="text-muted">
+                                                            {!edit && user.email}
+                                                            {edit && <input type="text" value={mail} onChange={(e) => setMail(e.target.value)} />}
+                                                        </p>
+                                                    </div>
+                                                    <div className="">
+                                                        <h6>Facebook</h6>
+                                                        {!edit && user.fbLink && <p className="text-muted">{user.fbLink}</p>}
+                                                        {!edit && !user.fbLink && <p className="text-muted">None</p>}
+                                                        {edit && <input type="text" value={fb} onChange={(e) => setFb(e.target.value)} />}
+                                                    </div>
+                                                    <div className="">
+                                                        <h6>LinkedIn</h6>
+                                                        {!edit && user.linkedinLink && <p className="text-muted">{user.linkedinLink}</p>}
+                                                        {!edit && !user.linkedinLink && <p className="text-muted">None</p>}
+                                                        {edit && <input type="text" value={lid} onChange={(e) => setLid(e.target.value)} />}
+                                                    </div>
+                                                    {edit && <div className="">
+                                                        <h6>Image</h6>
+                                                        <input type="text" value={image} placeholder="Insert drive link" onChange={(e) => setImage(e.target.value)} />
+                                                    </div>}
                                                 </div>
                                             </div>
                                         </div>
@@ -51,11 +152,11 @@ function Profile() {
                         </div>
                     </div>
                 </section>
-    
+
             </>
         )
     }
-    else{
+    else {
         return null;
     }
 }
